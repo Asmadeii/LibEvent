9ae1ab4b7e37f04ca5bbaf18b6d680d23cf038ae
asmadei <asmadei.ro@gmail.com>
2022-06-09 18:36:58 +0300

    <минус строчки>

--- UserControlDays.Designer.cs
+++ UserControlDays.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             this.lbdays = new System.Windows.Forms.Label();
-            this.text_1 = new System.Windows.Forms.Label();
-            this.text_2 = new System.Windows.Forms.Label();
-            this.text_3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbdays
@@ -45,44 +43,22 @@
             this.lbdays.Text = "01";
             this.lbdays.Click += new System.EventHandler(this.lbdays_Click);
             // 
-            // text_1
+            // label1
             // 
-            this.text_1.AutoSize = true;
-            this.text_1.Font = new System.Drawing.Font("Pobeda", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.text_1.Location = new System.Drawing.Point(36, 12);
-            this.text_1.Name = "text_1";
-            this.text_1.Size = new System.Drawing.Size(14, 17);
-            this.text_1.TabIndex = 1;
-            this.text_1.Text = "  ";
-            // 
-            // text_2
-            // 
-            this.text_2.AutoSize = true;
-            this.text_2.Font = new System.Drawing.Font("Pobeda", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.text_2.Location = new System.Drawing.Point(36, 34);
-            this.text_2.Name = "text_2";
-            this.text_2.Size = new System.Drawing.Size(17, 17);
-            this.text_2.TabIndex = 2;
-            this.text_2.Text = "   ";
-            // 
-            // text_3
-            // 
-            this.text_3.AutoSize = true;
-            this.text_3.Font = new System.Drawing.Font("Pobeda", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.text_3.Location = new System.Drawing.Point(36, 56);
-            this.text_3.Name = "text_3";
-            this.text_3.Size = new System.Drawing.Size(17, 17);
-            this.text_3.TabIndex = 3;
-            this.text_3.Text = "   ";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Pobeda", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(47, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // UserControlDays
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Linen;
-            this.Controls.Add(this.text_3);
-            this.Controls.Add(this.text_2);
-            this.Controls.Add(this.text_1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbdays);
             this.Name = "UserControlDays";
             this.Size = new System.Drawing.Size(140, 80);
@@ -95,8 +71,6 @@
         #endregion
 
         private Label lbdays;
-        private Label text_1;
-        private Label text_2;
-        private Label text_3;
+        private Label label1;
     }
 }
--- addEvCalendar.Designer.cs
+++ addEvCalendar.Designer.cs
@@ -28,69 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tEvBox = new System.Windows.Forms.TextBox();
-            this.sEvBox = new System.Windows.Forms.TextBox();
             this.fEvBox = new System.Windows.Forms.TextBox();
-            this.ev3 = new System.Windows.Forms.Label();
-            this.ev2 = new System.Windows.Forms.Label();
             this.ev1 = new System.Windows.Forms.Label();
             this.autoLabel = new System.Windows.Forms.Label();
             this.addEvButton = new System.Windows.Forms.Button();
             this.closeButton = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // tEvBox
-            // 
-            this.tEvBox.Location = new System.Drawing.Point(33, 283);
-            this.tEvBox.Name = "tEvBox";
-            this.tEvBox.Size = new System.Drawing.Size(281, 23);
-            this.tEvBox.TabIndex = 40;
-            this.tEvBox.Enter += new System.EventHandler(this.tEvBox_Enter);
-            this.tEvBox.Leave += new System.EventHandler(this.tEvBox_Leave);
-            // 
-            // sEvBox
-            // 
-            this.sEvBox.Location = new System.Drawing.Point(33, 214);
-            this.sEvBox.Name = "sEvBox";
-            this.sEvBox.Size = new System.Drawing.Size(281, 23);
-            this.sEvBox.TabIndex = 39;
-            this.sEvBox.Enter += new System.EventHandler(this.sEvBox_Enter);
-            this.sEvBox.Leave += new System.EventHandler(this.sEvBox_Leave);
-            // 
             // fEvBox
             // 
-            this.fEvBox.Location = new System.Drawing.Point(33, 145);
+            this.fEvBox.Location = new System.Drawing.Point(33, 168);
             this.fEvBox.Name = "fEvBox";
             this.fEvBox.Size = new System.Drawing.Size(281, 23);
             this.fEvBox.TabIndex = 38;
             this.fEvBox.Enter += new System.EventHandler(this.fEvBox_Enter);
             this.fEvBox.Leave += new System.EventHandler(this.fEvBox_Leave);
             // 
-            // ev3
-            // 
-            this.ev3.AutoSize = true;
-            this.ev3.Font = new System.Drawing.Font("Pobeda", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ev3.Location = new System.Drawing.Point(33, 247);
-            this.ev3.Name = "ev3";
-            this.ev3.Size = new System.Drawing.Size(101, 26);
-            this.ev3.TabIndex = 37;
-            this.ev3.Text = "Мероприятие";
-            // 
-            // ev2
-            // 
-            this.ev2.AutoSize = true;
-            this.ev2.Font = new System.Drawing.Font("Pobeda", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ev2.Location = new System.Drawing.Point(33, 178);
-            this.ev2.Name = "ev2";
-            this.ev2.Size = new System.Drawing.Size(101, 26);
-            this.ev2.TabIndex = 36;
-            this.ev2.Text = "Мероприятие";
-            // 
             // ev1
             // 
             this.ev1.AutoSize = true;
             this.ev1.Font = new System.Drawing.Font("Pobeda", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ev1.Location = new System.Drawing.Point(33, 109);
+            this.ev1.Location = new System.Drawing.Point(33, 132);
             this.ev1.Name = "ev1";
             this.ev1.Size = new System.Drawing.Size(101, 26);
             this.ev1.TabIndex = 35;
@@ -112,7 +70,7 @@
             // 
             this.addEvButton.AutoSize = true;
             this.addEvButton.Font = new System.Drawing.Font("Pobeda", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.addEvButton.Location = new System.Drawing.Point(109, 341);
+            this.addEvButton.Location = new System.Drawing.Point(111, 224);
             this.addEvButton.Name = "addEvButton";
             this.addEvButton.Size = new System.Drawing.Size(125, 36);
             this.addEvButton.TabIndex = 41;
@@ -138,14 +96,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(226)))), ((int)(((byte)(213)))));
-            this.ClientSize = new System.Drawing.Size(346, 390);
+            this.ClientSize = new System.Drawing.Size(346, 272);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.addEvButton);
-            this.Controls.Add(this.tEvBox);
-            this.Controls.Add(this.sEvBox);
             this.Controls.Add(this.fEvBox);
-            this.Controls.Add(this.ev3);
-            this.Controls.Add(this.ev2);
             this.Controls.Add(this.ev1);
             this.Controls.Add(this.autoLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -158,11 +112,7 @@
         }
 
         #endregion
-        private TextBox tEvBox;
-        private TextBox sEvBox;
         private TextBox fEvBox;
-        private Label ev3;
-        private Label ev2;
         private Label ev1;
         private Label autoLabel;
         private Button addEvButton;
