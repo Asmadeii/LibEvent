6d609f0a7b22dc24085890a16080ed2200cda0c0
asmadei <asmadei.ro@gmail.com>
2022-06-14 20:28:52 +0300

    <add class>

---
+++ CalendarNotes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibEvent
+{
+    public class CalendarNote
+    {
+        public int year;
+        public int month;
+        public int day;
+        public string note;
+    }
+    public class CalendarNotes
+    {
+        private static List<CalendarNote> _notes = new List<CalendarNote>();
+        public static void AddNote(CalendarNote note)
+        {
+            _notes.Add(note);
+        }
+    }
+}
